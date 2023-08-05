43bdbda793c480f926a834817cbd05f82003553c
include07 <j.elfirqi@gmail.com>
2023-08-05 01:25:07 +0100

    change right sidebar and add left sidebar

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 let menu = document.querySelector("#menu-logo");
 let sidebar = document.querySelector(".sidebar-container");
+let menuLeft = document.querySelector("#club-logo");
+let sidebarLeft = document.querySelector(".sidebar-menu");
 if(menu){
     menu.addEventListener('click', function(e){
         if(menu.getAttribute('data-state')=='1'){
-            menu.setAttribute('src','./images/close.png');
             menu.setAttribute('data-state','2');
             sidebar.style.display="block";
 
         }else{
-            menu.setAttribute('src','./images/menu.png');
             menu.setAttribute('data-state','1');
             sidebar.style.display="none";
+        }
+    })
+}
+if(menuLeft){
+    menuLeft.addEventListener('click', function(e){
+        if(menuLeft.getAttribute('data-state')=='1'){
+            menuLeft.setAttribute('data-state','2');
+            sidebarLeft.style.display="block";
+
+        }else{
+            menuLeft.setAttribute('data-state','1');
+            sidebarLeft.style.display="none";
         }
     })
 }
